9380338bf1743527f6b66a18c5ca09ce89e9cef5
Eduardo Garcia <eduardogarciap@outlook.com>
2024-07-20 20:01:06 -0300

    adicionando funcionalides pesquisa_servico

--- js/pesquisas/pesquisaServico.js
+++ js/pesquisas/pesquisaServico.js
@@ -1,5 +1,41 @@
-let retornarMenu = document.querySelector('.ret-pesquisa');
+if (typeof retornarMenu === 'undefined' || retornarMenu === null) {
+    var retornarMenu = document.querySelector('.ret-pesquisa');
+}
 retornarMenu.addEventListener('click', function(e){
     e.preventDefault();
     carregarPagina(paginas['pesquisas'], main);
 });
+document.querySelector('table.pesquisa_servico').addEventListener('click', function(e){
+    e.preventDefault();
+    if(e.target.classList.contains('historico_servico')){
+        fetch(`frontend/pesquisas/servico/historicoServico.html`).then(function (response) {
+            return response.text();
+        }).then(function (html) {
+            modalTelas.title_.innerText = 'Histórico Serviço';
+            modalTelas.body_.innerHTML = html;
+            modalTelas.show();
+            
+        }).catch(function (err) {
+            console.log('Erro carregar a página ', err);
+        });
+    }
+    if(e.target.classList.contains('detalhes_servico')){
+        fetch(`frontend/pesquisas/servico/detalheServico.html`).then(function (response){
+            return response.text();
+        }).then(function (html){
+            modalTelas.title_.innerText = 'Detalhes Serviço';
+            modalTelas.body_.innerHTML = html;
+            modalTelas.show();
+        }).catch(function (err){
+            console.log('Erro carregar a página', err);
+        })
+    }
+    if(e.target.classList.contains('excluir_servico')){
+        modalTelas.title_.innerText = 'Remover Serviço';
+        modalTelas.body_.innerHTML = `<div class="d-block">
+                                        <div> Deseja realmente remover o Serviço [nome]?</div>
+                                      <div><button class="btn btn-danger me-2">Sim</button><button class="btn btn-primary">Cancelar</button></div></div>`;
+        modalTelas.show();
+                                      
+    }
+})
